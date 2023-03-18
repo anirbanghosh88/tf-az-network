d9cee20aad2a7a2f19b0a4648a6e5bd91cc2cf77
anirbanghosh88 <108187140+anirbanghosh88@users.noreply.github.com>
2023-03-19 00:17:18 +0530

    Revert "Module/network"

---
+++ main.tf
@@ -0,0 +1,20 @@
+# Create a resource group
+resource "azurerm_resource_group" "this" {
+  name     = "demo-rg01"
+  location = "eastus"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "this" {
+  name                = "demo-vnet01"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  address_space       = ["10.2.0.0/16"]
+}
+
+resource "azurerm_subnet" "this" {
+  name                 = "demo-sub01"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.2.1.0/24"]
+}
---
+++ module/network/variable.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "address_space" {
+  type = string
+}  
