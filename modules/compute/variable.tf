01d140772e6536335e6c901c2f286fdc904d4e23
Anirban <anirbanghosh88@gmail.com>
2023-03-19 19:06:28 +0530

    compute module

---
+++ modules/compute/variable.tf
@@ -0,0 +1,21 @@
+variable "environment" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "rg_name" {
+  type = string
+}
+variable "vmname" {
+  type = string
+}
+variable "vmcount" {
+  type = string
+}
+variable "vmsize" {
+  type = string
+}
+variable "subnet_id" {
+  type = string
+}
